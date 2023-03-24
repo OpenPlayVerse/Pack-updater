a00e9b1f7f528a4f9043517474c41d6b8db33e2c
MisterNoNameLP <misternonamelp@web.de>
2023-03-24 20:59:00 +0100

    bug fix

--- runServer.sh
+++ runServer.sh
@@ -10,7 +10,7 @@ echo 1 > $serverRunnerInstructionFile
 while [ ! $(cat $serverRunnerInstructionFile) -eq 0 ]; do
     if [ $(cat $serverRunnerInstructionFile) -eq 1 ]; then
         echo 1 > $serverStatusFile
-	    sh ./serverStart.sh
+	    ./$serverStartScript
 	    echo "RESTART SERVER"
         echo 0 > $serverStatusFile
     elif [ $(cat $serverRunnerInstructionFile) -eq 2 ]; then
